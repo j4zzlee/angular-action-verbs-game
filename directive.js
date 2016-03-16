0a15ff4e775db234fb4620fe12da0a3b50fa7e2f
st2forget <hoanggia.lh@gmail.com>
2016-03-16 14:10:39 +0700

    asfsafsaf

--- directive.js
+++ directive.js
@@ -6,7 +6,7 @@ angular.module('st2forget.action-verbs-game', [])
 
         return {
             controllerAs: 'actionVerbsGame',
-            controller  : ['$attrs', '$scope', '$element', '$css', function ($attrs, $scope, $element, $css) {
+            controller  : ['$attrs', '$scope', '$element', function ($attrs, $scope, $element) {
                 $scope.libPath = $attrs.libPath;
                 $scope.model   = {
                     description: 'Practice Action Verbs Vocabulary with this ESL  Memory Game. This game is also excellent for Vocabulary Teaching and Practice. ESL Learners and Teachers can use it to review English vocabulary or simply practice these words. This memory games has audio, images and text which makes it possible to practice spelling, reading, listening and word recognition. It is great for beginner and elementary ESL Lesson Plans.',
@@ -49,14 +49,10 @@ angular.module('st2forget.action-verbs-game', [])
                 $scope.itemClick = function (e) {
                     console.log(e.target);
                 };
-
-                $css.add('lib/angular-action-verbs-game/action-verbs.css');
-                console.log('asfsdfsdf');
             }],
             template    : '<div class="action-verbs-item" ng-click="itemClick($event)" ng-repeat="question in model.questions">' +
             '{{ question.statement }}' +
             "</div>",
-            link        : link,
-            css         : 'lib/angular-action-verbs-game/action-verbs.css'
+            link        : link
         };
     });
